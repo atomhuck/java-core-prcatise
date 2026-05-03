3a321f9d7d8083dcd0af3ef137c33b17948090c7
atomhuck <142097453+atomhuck@users.noreply.github.com>
2026-05-03 19:46:26 +0300

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task15.task1516;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Файл или директория
+*/
+
+public class Solution {
+    private static final String THIS_IS_FILE = " - это файл";
+    private static final String THIS_IS_DIR = " - это директория";
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        while(true){
+            String str = console.nextLine();
+            Path path = Path.of(str);
+            if (Files.isRegularFile(path))
+                System.out.println(str + THIS_IS_FILE);
+            else if (Files.isDirectory(path))
+                System.out.println(str + THIS_IS_DIR);
+            else
+                break;
+        }
+    }
+}
+
