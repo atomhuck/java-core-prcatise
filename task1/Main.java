340480399cde86f2aa5ec9a10078dbf1df2923f0
Efim <142097453+atomhuck@users.noreply.github.com>
2026-05-08 23:58:31 +0300

    Refactor task list initialization using List.of

--- task1/Main.java
+++ task1/Main.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<DeliveryTask> tasks = new ArrayList<>();
-        tasks.add(new StandardDelivery(100, 5, 2));
-        tasks.add(new ExpressDelivery(100, 5, 2));
-        tasks.add(new HeavyCargoDelivery(100, 5, 20));
+        List<DeliveryTask> tasks = List.of(
+                new StandardDelivery(100, 5, 2),
+                new ExpressDelivery(100, 5, 2),
+                new HeavyCargoDelivery(100, 5, 20)
+        );
 
         Dispatcher dispatcher = new Dispatcher();
 
